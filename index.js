7472dbf7dd5524b5692e70628b2e8924b29f280f
Rodrigo Brolese <rodrigobrolese0@gmail.com>
2021-02-15 20:50:36 -0300

    Criado estrutura do projeto

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+var { Liquid } = require('liquidjs')
+const path = require('path')
+
+const app = express()
+var engine = new Liquid()
+
+app.engine('liquid', engine.express())
+app.set('views', './views')
+app.set('view engine', 'liquid')
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+const server = app.listen(3010, () => {
+  console.log(`The application started on port ${server.address().port}`)
+});
